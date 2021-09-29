554cf39d885059a269f38e9d37051a7baa73c034
Eugene <nester77722@gmail.com>
2021-09-30 00:49:01 +0300

    Created DBContext

---
+++ Drugstore/DAL/DrugstoreDBContext.cs
@@ -0,0 +1,32 @@
+﻿using Microsoft.EntityFrameworkCore;
+using DAL.Entities;
+using DAL.Configurations;
+using DAL.Entities.Order;
+using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+using Microsoft.AspNetCore.Identity;
+
+namespace DAL
+{
+    public class DrugstoreDBContext : IdentityDbContext
+    {
+        public DrugstoreDBContext(DbContextOptions<DrugstoreDBContext> options) : base(options)
+        {
+        }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            modelBuilder.ApplyConfiguration(new StoreConfig());
+            modelBuilder.ApplyConfiguration(new OrderConfig());
+
+            base.OnModelCreating(modelBuilder);
+        }
+        public DbSet<RefreshToken> Tokens { get; set; }
+        public DbSet<Brand> Brands { get; set; }
+        public DbSet<ProductImage> ProductImages { get; set; }
+        public DbSet<Product> Products { get; set; }
+        public DbSet<Stock> Stocks { get; set; }
+        public DbSet<Store> Stores { get; set; }
+        public DbSet<Order> Orders { get; set; }
+        public DbSet<OrderItem> OrderItems { get; set; }
+    }
+}
