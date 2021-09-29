8173dd20d82300a3ab362c1bb8ad4260684da1a2
Eugene <nester77722@gmail.com>
2021-09-30 00:49:20 +0300

    Created RefreshToken entity

---
+++ Drugstore/DAL/Entities/RefreshToken.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.AspNetCore.Identity;
+
+namespace DAL.Entities
+{
+    public class RefreshToken
+    {
+        public int Id { get; set; }
+
+        public string Value { get; set; }
+
+        public string IdentityUserId { get; set; }
+        public IdentityUser IdentityUser { get; set; }
+    }
+}
