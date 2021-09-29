00091ee410012031d8f1b0b885714f6b2af35a8d
Eugene <nester77722@gmail.com>
2021-09-30 00:52:08 +0300

    Added configs for Jwt

---
+++ Drugstore/PL/Configuration/TokenLifeTimeValidator.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using Microsoft.IdentityModel.Tokens;
+
+namespace PL.Configuration
+{
+    public static class TokenLifetimeValidator
+    {
+        public static bool Validate(
+            DateTime? notBefore,
+            DateTime? expires,
+            SecurityToken tokenToValidate,
+            TokenValidationParameters @param
+        )
+        {
+            return (expires != null && expires > DateTime.UtcNow);
+        }
+    }
+}
