e2555018e3100bde612c3713a6c015f70fff2b6a
Eugene <nester77722@gmail.com>
2021-09-30 00:47:29 +0300

    Created entities for Product

---
+++ Drugstore/DAL/Entities/Product.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DAL.Entities
+{
+    public class Product
+    {
+        [Required]
+        public int Id { get; set; }
+
+        [Required]
+        public string Name { get; set; }
+
+        public int BrandId { get; set; }
+        public Brand Brand { get; set; }
+
+        [Required]
+        public int Price { get; set; }
+        public string Description { get; set; }
+    }
+}
---
+++ Drugstore/DAL/Entities/ProductImage.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DAL.Entities
+{
+    public class ProductImage
+    {
+        [Required]
+        [Key]
+        public int Id { get; set; }
+        [Required]
+        public string Path { get; set; }
+
+        [Required]
+        public int ProductId { get; set; }
+        [ForeignKey("ProductId")]
+        public Product Product { get; set; }
+    }
+}
