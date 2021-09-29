89f97402317cc8ca2d4141cedfad185175cd1113
Eugene <nester77722@gmail.com>
2021-09-30 00:48:05 +0300

    Created Stock and Store entities

---
+++ Drugstore/DAL/Entities/Store.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DAL.Entities
+{
+    public class Store
+    {
+        [Required]
+        public int Id { get; set; }
+
+        public string Name { get; set; }
+
+        public string Phone { get; set; }
+
+        [Required]
+        public string Address { get; set; }
+
+        public ICollection<Stock> Stocks { get; set; }
+    }
+}
