a5b933e6801bb085e13b8f70374867d6df730baf
Eugene <nester77722@gmail.com>
2021-09-30 00:54:45 +0300

    Added MappingProfile

---
+++ Drugstore/PL/MappingProfile.cs
@@ -0,0 +1,28 @@
+﻿    using AutoMapper;
+using BLL.Models;
+using DAL.Entities;
+using PL.ViewModels;
+
+namespace PL
+{
+    public class MappingProfile : Profile
+    {
+        public MappingProfile()
+        {
+            CreateMap<Store, StoreModel>();
+            CreateMap<StoreModel, StoreViewModel>();
+
+            CreateMap<Product, ProductModel>();
+            CreateMap<ProductModel, ProductViewModel>();
+
+            CreateMap<StoreResponse, StoreResponseModel>();
+            CreateMap<StoreResponseModel, StoreResponseViewModel>();
+
+            CreateMap<ProductResponse, ProductResponseModel>();
+            CreateMap<ProductResponseModel, ProductResponseViewModel>();
+
+            CreateMap<ProductImage, ProductImageModel>();
+            CreateMap<ProductImageModel, ProductImageViewModel>();
+        }
+    }
+}
