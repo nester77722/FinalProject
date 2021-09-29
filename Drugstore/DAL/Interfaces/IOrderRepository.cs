7c1ebbc420f9393a00da7c4219fc94a2c03a3934
Eugene <nester77722@gmail.com>
2021-09-30 00:50:22 +0300

    Created Sortings and Interfaces for future Repo

---
+++ Drugstore/DAL/Interfaces/IOrderRepository.cs
@@ -0,0 +1,15 @@
+﻿using DAL.Entities.Order;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DAL.Interfaces
+{
+    public interface IOrderRepository
+    {
+        Task<OrderResponse> GetOrdersAsync(int page, int pageSize, string userName);
+        Task<Order> GetOrderByIdAsync(int id);
+    }
+}
---
+++ Drugstore/DAL/Interfaces/IProductImageRepository.cs
@@ -0,0 +1,11 @@
+﻿using System.Collections.Generic;
+using System.Threading.Tasks;
+using DAL.Entities;
+
+namespace DAL.Interfaces
+{
+    public interface IProductImageRepository
+    {
+        Task<ProductImage> GetProductImageAsync(int productId);
+    }
+}
