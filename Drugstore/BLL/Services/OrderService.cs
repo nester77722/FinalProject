d8923a4444038d008394455bd86ace6fe3bd7b19
Eugene <nester77722@gmail.com>
2021-09-30 00:51:47 +0300

    Created services

---
+++ Drugstore/BLL/Services/OrderService.cs
@@ -0,0 +1,35 @@
+﻿using AutoMapper;
+using BLL.Interfaces;
+using BLL.Models;
+using DAL.Interfaces;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BLL.Services
+{
+    public class OrderService : IOrderService
+    {
+        private readonly IOrderRepository _orderRepository;
+        private readonly IMapper _mapper;
+
+        public OrderService(IOrderRepository ordertRepository, IMapper mapper)
+        {
+            _orderRepository = ordertRepository;
+            _mapper = mapper;
+        }
+        public async Task<OrderModel> GetOrderAsync(int id)
+        {
+            var order = await _orderRepository.GetOrderByIdAsync(id);
+            return _mapper.Map<OrderModel>(order);
+        }
+
+        public async Task<OrderResponseModel> GetOrdersAsync(int page, int pageSize, string userId)
+        {
+            var orders = await _orderRepository.GetOrdersAsync(page, pageSize, userId);
+            return _mapper.Map<OrderResponseModel>(orders);
+        }
+    }
+}
---
+++ Drugstore/BLL/Services/StoreService.cs
@@ -0,0 +1,35 @@
+﻿using AutoMapper;
+using BLL.Interfaces;
+using BLL.Models;
+using DAL.Interfaces;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BLL.Services
+{
+    public class StoreService : IStoreService
+    {
+        private readonly IStoreRepository _storeRepository;
+        private readonly IMapper _mapper;
+
+        public StoreService(IStoreRepository storeRepository, IMapper mapper)
+        {
+            _storeRepository = storeRepository;
+            _mapper = mapper;
+        }
+        public async Task<StoreModel> GetStoreAsync(int id)
+        {
+            var store = await _storeRepository.GetStoreByIdAsync(id);
+            return _mapper.Map<StoreModel>(store);
+        }
+
+        public async Task<StoreResponseModel> GetStoresAsync(int page, int pageSize)
+        {
+            var stores = await _storeRepository.GetStoresAsync(page, pageSize);
+            return _mapper.Map<StoreResponseModel>(stores);
+        }
+    }
+}
