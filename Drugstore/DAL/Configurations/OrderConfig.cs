22885421f8c3f5aed0aad445e162da0a16df3cf2
Eugene <nester77722@gmail.com>
2021-09-30 00:49:43 +0300

    Added Configurations for Orders and Stores

---
+++ Drugstore/DAL/Configurations/OrderConfig.cs
@@ -0,0 +1,20 @@
+﻿using DAL.Entities.Order;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DAL.Configurations
+{
+    public class OrderConfig : IEntityTypeConfiguration<Order>
+    {
+        public void Configure(EntityTypeBuilder<Order> builder)
+        {
+            builder.HasMany(o => o.OrderItems)
+                .WithOne(o => o.Order);
+        }
+    }
+}
