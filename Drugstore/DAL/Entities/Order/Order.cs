ce74a335a562622c4d26289c092e673335120ead
Eugene <nester77722@gmail.com>
2021-09-30 00:48:20 +0300

    Created Order entities

---
+++ Drugstore/DAL/Entities/Order/Order.cs
@@ -0,0 +1,18 @@
+﻿using Microsoft.AspNetCore.Identity;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DAL.Entities.Order
+{
+    public class Order
+    {
+        public int Id { get; set; }
+        public DateTime DateTime { get; set; }
+        public string UserName { get; set; }
+        public IdentityUser User { get; set; }
+        public ICollection<OrderItem> OrderItems { get; set; }
+    }
+}
