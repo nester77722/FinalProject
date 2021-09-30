091bbee03abc062e46df786f50a9eae22d50a721
Eugene <nester77722@gmail.com>
2021-09-30 12:15:18 +0300

    ChangesForOrder and stores

--- Drugstore/BLL/Services/OrderService.cs
+++ Drugstore/BLL/Services/OrderService.cs
@@ -1,6 +1,7 @@
 ﻿using AutoMapper;
 using BLL.Interfaces;
 using BLL.Models;
+using DAL.Entities.Order;
 using DAL.Interfaces;
 using System;
 using System.Collections.Generic;
@@ -20,16 +21,35 @@ namespace BLL.Services
             _orderRepository = ordertRepository;
             _mapper = mapper;
         }
+
+        public async Task<OrderModel> AddOrderAsync(OrderModel order)
+        {
+            var orderEntity = _mapper.Map<Order>(order);
+            return _mapper.Map<OrderModel>(await _orderRepository.AddOrderAsync(orderEntity));
+        }
+
         public async Task<OrderModel> GetOrderAsync(int id)
         {
             var order = await _orderRepository.GetOrderByIdAsync(id);
             return _mapper.Map<OrderModel>(order);
         }
 
-        public async Task<OrderResponseModel> GetOrdersAsync(int page, int pageSize, string userId)
+        public async Task<OrderResponseModel> GetOrdersAsync(int page, int pageSize, string userName)
         {
-            var orders = await _orderRepository.GetOrdersAsync(page, pageSize, userId);
-            return _mapper.Map<OrderResponseModel>(orders);
+            var orders = await _orderRepository.GetOrdersAsync(page, pageSize, userName);
+
+            var result = _mapper.Map<OrderResponseModel>(orders);
+            foreach (var order in result.Orders)
+            {
+                order.OrderItems = await GetOrderItemModelsAsync(order.Id);
+            }
+            return result;
+        }
+
+        private async Task<ICollection<OrderItemModel>> GetOrderItemModelsAsync(int orderId)
+        {
+            var orderItems = await _orderRepository.GetOrderItems(orderId);
+            return _mapper.Map<ICollection<OrderItemModel>>(orderItems);
         }
     }
 }
