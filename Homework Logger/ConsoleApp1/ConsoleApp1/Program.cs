7abbcba1983b45410932fb65e9f4b24bd7491e0d
Vazhynsky <nester77722@gmail.com>
2021-11-17 13:03:43 +0200

    Added work with logger

---
+++ Homework Logger/ConsoleApp1/ConsoleApp1/Program.cs
@@ -0,0 +1,14 @@
+﻿using System;
+
+namespace ConsoleApp1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Logger logger = new Logger();
+            logger.AddLog("TestLog");
+            Console.WriteLine(logger.Logs);
+        }
+    }
+}
