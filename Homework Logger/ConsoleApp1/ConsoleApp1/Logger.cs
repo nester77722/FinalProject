2cb87cbb73bdc671a096cc0f614477ff7d5c631d
Vazhynsky <nester77722@gmail.com>
2021-11-17 13:03:29 +0200

    Added logger

---
+++ Homework Logger/ConsoleApp1/ConsoleApp1/Logger.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp1
+{
+    public class Logger
+    {
+        private string _logs = string.Empty;
+        public string Logs 
+        {
+            get
+            {
+                return _logs;
+            }
+        }
+        public void AddLog(string message)
+        {
+            _logs += $"\n{DateTime.Now}: {message}";
+        }
+    }
+}
